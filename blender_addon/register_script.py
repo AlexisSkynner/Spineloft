2c1bd15eb63e4a026d93a054eb768e98dce0244c
Timothée Février <timothee.fevrier@telecom-paris.fr>
2025-05-05 12:25:21 +0000

    Edit register_script.py Supprimer l'appel de la class SpineDrawer qui ne sert plus

--- blender_addon/register_script.py
+++ blender_addon/register_script.py
@@ -5,7 +5,6 @@ from . import custom_editor
 # Initialisation
 def register_all():
     bpy.utils.register_class(ui.Panel_PhotoSketching)
-    bpy.utils.register_class(ui.Panel_Spinedrawer)
     bpy.utils.register_class(ui.Operator_CreateVolume)
     bpy.utils.register_class(ui.Operator_DrawSpine)
     bpy.utils.register_class(custom_editor.Operator_UImanager)
@@ -14,7 +13,6 @@ def register_all():
 
 def unregister_all():
     bpy.utils.unregister_class(ui.Panel_PhotoSketching)
-    bpy.utils.unregister_class(ui.Panel_Spinedrawer)
     bpy.utils.unregister_class(ui.Operator_CreateVolume)
     bpy.utils.unregister_class(ui.Operator_DrawSpine)
     bpy.utils.unregister_class(custom_editor.Operator_UImanager)
