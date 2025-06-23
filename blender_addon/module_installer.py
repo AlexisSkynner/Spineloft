b807e7ee704643b6b45b86093580f6f2170a5ae2
Skynner <alexis.rabot@telecom-paris.fr>
2025-06-23 15:53:44 +0200

    Pip et pillow importation fonctionnel sur fedora à tester sur autre

--- blender_addon/module_installer.py
+++ blender_addon/module_installer.py
@@ -4,42 +4,39 @@ import sys
 import bpy
 import importlib
 
-
-path=bpy.utils.user_resource("SCRIPTS", path="modules")
-sys.path.append(path)
+path = bpy.utils.user_resource("SCRIPTS", path="modules")
+if path not in sys.path:
+    sys.path.append(path)
 
 def printError(self, context):
     self.layout.label(text="Required modules cannot be installed. Try again while running Blender as administrator. Desired path = "+path)
 
 
-#pip
+# pip
 try:
     import pip
     print("pip is already installed")
 
-except:
+except ImportError:
     print("pip is not available in "+path+". Installation...")
     try:
         import ensurepip
         ensurepip.bootstrap()
-    except:
-        print("pip" \ cannot be installed in "+path+". Try running Blender as administrator.")
+    except Exception:
+        print("pip cannot be installed in "+path+". Try running Blender as administrator.")
         bpy.context.window_manager.popup_menu(printError, title="Error", icon='ERROR')
 
-#pillow
+# Pillow et import explicite de Image
 try:
-    import PIL
-    print("Pillow is already installed")
+    from PIL import Image
+    print("Pillow (Image) is already installed")
 
-except:
+except ImportError:
     print("Pillow is not available in "+path+". Installation...")
     try:
         subprocess.check_call([sys.executable, "-m", "pip", "install", "Pillow"])
         importlib.invalidate_caches()
-        import PIL
-    except:
-        print("pip" \ cannot be installed in "+path+". Try running Blender as administrator.")
+        from PIL import Image
+    except Exception:
+        print("Pillow cannot be installed in "+path+". Try running Blender as administrator.")
         bpy.context.window_manager.popup_menu(printError, title="Error", icon='ERROR')
-
-
-####################################
