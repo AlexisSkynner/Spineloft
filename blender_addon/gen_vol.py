916cfd03a05943314f07bd531b195ca9c7d8fbd2
Dorian Geais <dorian.geais@telecom-paris.fr>
2025-06-23 10:41:55 +0200

    Résolution bug gen_vol.py (gestion des angles supérieurs à pi)

--- blender_addon/gen_vol.py
+++ blender_addon/gen_vol.py
@@ -1,6 +1,5 @@
 import copy
 import math
-import numpy as np
 import bpy
 
 
@@ -11,22 +10,56 @@ class Shape():
         self.edges = edges
         self.faces = faces
 
-def angle_between_vectors(u, v):
+def cos_sin_between_vectors(u, v):
     dot_product = sum(i*j for i, j in zip(u, v))
+    vect_product = u[0]*v[1]-u[1]*v[0]
     norm_u = math.sqrt(sum(i**2 for i in u))
     norm_v = math.sqrt(sum(i**2 for i in v))
     cos_theta = dot_product / (norm_u * norm_v)
-    angle_rad = math.acos(cos_theta)
-    return angle_rad
+    sin_theta = vect_product / (norm_u * norm_v)
+    return (cos_theta,sin_theta)
+
+
+
 
 def norm(u):
     return(math.sqrt(sum(i**2 for i in u)))
 
-def rot(shape:Shape,theta:float):
-    c=np.cos(theta)
-    s=np.sin(theta)
 
-    mat=np.array([[c,-s,0],[s,c,0],[0,0,1]])
+
+def sum_matrices(A,B):
+    C=[]
+    for i in range(len(A)):
+        C.append(A[i]+B[i])
+    return(C)
+
+
+def diff_matrices(A,B):
+    C=[]
+    for i in range(len(A)):
+        C.append(A[i]-B[i])
+    return(C)
+
+def dot(matrice, vecteur):
+    n = len(matrice)
+    resultat = []
+
+    for i in range(n):
+        somme = 0
+        for j in range(n):
+            produit = matrice[i][j] * vecteur[j]
+            somme = somme + produit
+        resultat.append(somme)
+
+    return resultat
+
+
+
+def rot(shape:Shape,cos_sin_theta:float):
+    c=cos_sin_theta[0]
+    s=cos_sin_theta[1]
+
+    mat=[[c,s,0],[-s,c,0],[0,0,1]]
 
 
     shape2=copy.deepcopy(shape)
@@ -34,20 +67,20 @@ def rot(shape:Shape,theta:float):
     
 
     for i in range(len(vertices)):
-        col=np.array(vertices[i]).T
-        res=np.dot(mat,col).T
+        col=list(vertices[i])
+        res=dot(mat,col)
         vertices[i]=tuple(res)
 
- 
     return(shape2)
 
 def homo(shape:Shape,h:float):
     shape2=copy.deepcopy(shape)
-    vertices=shape2.vertices
+    vertices=list(shape2.vertices)
 
 
     for i in range(len(vertices)):
-        vertices[i]=tuple(h*np.array( vertices[i] ))
+        vertices[i]=tuple([x * h for x in vertices[i]])
+    shape2.vertices=vertices
     return(shape2)
 
 def transl(shape:Shape,vect:tuple):
@@ -55,55 +88,12 @@ def transl(shape:Shape,vect:tuple):
     vertices=shape2.vertices
     
     for i in range(len(vertices)):
-        vertices[i]=tuple(np.array(vertices[i]) + np.array(vect))
+        vertices[i]=tuple(sum_matrices(list(vertices[i]), list(vect)))
     return(shape2)
 
 def middle(p1:tuple,p2:tuple):
     return ( ( (p1[0]+p2[0])/2 , (p1[1]+p2[1])/2 , (p1[2]+p2[2])/2))
 
-def printVertices(shape:Shape):
-    X=[]
-    Y=[]
-    Z=[]
-
-    for u in shape.vertices:
-        X.append(u[0])
-        Y.append(u[1])
-        Z.append(u[2])
-
-    ax = plt.axes(projection='3d')
-    ax.set_xlabel('X axis')
-    ax.set_ylabel('Y axis')
-    ax.set_zlabel('Z axis')
-    ax.plot3D(X,Y,Z, 'o:r',linestyle='None')
-    plt.show()
-
-def printEdges(shape:Shape):
-    ax = plt.axes(projection='3d')
-
-    for edge in shape.edges:
-    
-        X=[]
-        Y=[]
-        Z=[]
-
-        j1,j2=edge[0],edge[1]
-        vertices=shape.vertices
-
-        for j in [j1,j2]:
-            X.append(vertices[j][0])
-            Y.append(vertices[j][1])
-            Z.append(vertices[j][2])
-
-        ax.plot3D(X,Y,Z, 'b')
-
-    ax.set_xlim(0,4)
-    ax.set_ylim(0,4)
-    ax.set_zlim(-2,2)
-    ax.set_xlabel('X axis')
-    ax.set_ylabel('Y axis')
-    ax.set_zlabel('Z axis')
-    plt.show()    
 
 
 
@@ -124,6 +114,12 @@ C=[]
 
 shapeHeart=copy.deepcopy(homo(Shape(A,B,C),0.25))
 
+#Circle
+A=[(math.cos(2*math.pi*k/20), 0, math.sin(2*math.pi*k/20)) for k in range(20)] #Il faut une figure qui fait face vers les y (y=0 pour tout point), dont le centre est en 0,0,0, de largeur 1
+B=[(0,19)]+[(i,i+1) for i in range(19)]
+C=[]
+
+shapeCircle=copy.deepcopy(homo(Shape(A,B,C),0.25))
 
 #Square
 A=[(-0.5,0,-0.5),(0.5,0,-0.5),(0.5,0,0.5),(-0.5,0,0.5)]
@@ -132,14 +128,6 @@ C=[]
 
 shapeSquare=copy.deepcopy(Shape(A,B,C))
 
-#Circle
-n=20
-X=np.arange(0,n)
-A=[(0.5*np.cos(2*np.pi*x/n) , 0, 0.5*np.sin(2*np.pi*x/n)) for x in X]
-B=[(0,n-1)]+[(i,i+1) for i in range (n-1)]
-C=[]
-
-shapeCircle=copy.deepcopy(Shape(A,B,C))
 
 
 
@@ -155,8 +143,8 @@ def addVertices(startShape:Shape,listPts:list):
     listDist=[]
     for i in range(l):
         listMid.append(middle(listPts[i][0],listPts[i][1]))
-        listAngl.append(angle_between_vectors(np.array(listPts[i][0])-np.array(listPts[i][1]) , (1,0,0) ))
-        listDist.append(norm(np.array(listPts[i][0])-np.array(listPts[i][1])))
+        listAngl.append(cos_sin_between_vectors(diff_matrices(list(listPts[i][1]),list(listPts[i][0])) , (1,0,0) ))
+        listDist.append(norm(diff_matrices(list(listPts[i][0]),list(listPts[i][1]))))
 
     #On trace la forme finale
     finalShape=Shape([],[],[])
@@ -174,10 +162,10 @@ def addEdges(finalShape:Shape,startShape:Shape, listPts:list):
     #Edges de la forme de départ
     for i in range( l ):
         for j in range(nbEdges):
-            edge=np.array(startShape.edges[j])
-            edge[0]=edge[0]+i*nbVertices
-            edge[1]=edge[1]+i*nbVertices
-            finalShape.edges.append(tuple(edge))
+            edge=list(startShape.edges[j])
+            edge0=edge[0]+i*nbVertices
+            edge1=edge[1]+i*nbVertices
+            finalShape.edges.append((edge0,edge1))
             
     #Edges et faces de la connection
     for i in range(l-1):
@@ -207,10 +195,10 @@ def addFaces(finalShape:Shape,startShape:Shape, listPts:list):
     lastFace=[]  
     for i in range(nbVertices):
         firstFace.append(i)
-        lastFace.append(nbVertices*l-i-1)
-    
+        #lastFace.append(nbVertices*(l-1)+i) 
+
     finalShape.faces.append(tuple(firstFace))
-    finalShape.faces.append(tuple(lastFace))
+    #finalShape.faces.append(tuple(lastFace))
     
     return(finalShape)
 
@@ -220,10 +208,15 @@ def addFaces(finalShape:Shape,startShape:Shape, listPts:list):
 
 
 def giveMeTheMesh(listPts):
-
-    finalShape=addVertices(shapeStart,listPts)
-    finalShape=addEdges(finalShape,shapeStart, listPts)
-    finalShape=addFaces(finalShape,shapeStart, listPts)
+    
+    newList=[]
+    for i in range(len(listPts)//2):
+        newList.append([listPts[2*i]+[0],listPts[2*i+1]+[0]])
+    
+    finalShape=addVertices(shapeStart,newList)
+    finalShape=addEdges(finalShape,shapeStart, newList)
+    finalShape=addFaces(finalShape,shapeStart, newList)
+    
 
     return(finalShape.vertices,finalShape.edges,finalShape.faces)
 
