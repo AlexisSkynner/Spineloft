1fdfaad69820ea399622c30cdac52894c8076ad3
AlexisRabot <alexisraboyt@gmail.com>
2025-09-26 09:18:31 +0200

    Delete test_d2 directory

--- test_d2/stroking.py
+++
@@ -1,7 +0,0 @@
-with open("stroke.txt", "w") as f:
-    for i in range(2, 48):
-        f.write(f"2 {i}")
-        f.write("\n")
-    for i in range(2, 48):
-        f.write(f"{i} 48")
-        f.write("\n")
